56966f0c34d0db30dbf175af908dc113c01aba1d
sqeezy <sqeezy@gmx.de>
2013-12-07 02:02:17 +0100

    Refactored classes, project and solution.

---
+++ Dijkstra/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
